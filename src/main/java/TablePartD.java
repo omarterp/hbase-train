8ee512b39dc9c60a70e4ca20894e97e4dee0fee0
Eric Perales <omarterp@gmail.com>
2017-03-11 22:23:39 -0500

    starting PartD

--- src/main/java/TablePartD.java
+++ src/main/java/TablePartD.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.*;
 
 import org.apache.hadoop.conf.Configuration;
 
@@ -19,9 +20,34 @@ import org.apache.hadoop.hbase.util.Bytes;
 
 public class TablePartD{
 
-   public static void main(String[] args) throws IOException {
+    private final Configuration conf = HBaseConfiguration.create();
+    private HBaseAdmin admin;
 
-	//TODO      
-   }
+    public TablePartD(String ip, String port) throws IOException {
+        setHbaseConf(ip, port);
+        admin = new HBaseAdmin(conf);
+    }
+
+    private void setHbaseConf(String ip, String port) {
+        conf.set("hbase.zookeeper.quorum", ip);
+        conf.set("hbase.zookeeper.property.clientport", port);
+        conf.set("zookeeper.znode.parent", "/hbase-unsecure");
+    }
+
+    public List<byte[]> getHTableRow(String tableName, Map.Entry<String, Set<String>> entry) throws IOException {
+        List<byte[]> hTableRow = new ArrayList<>();
+
+        HTable hTable = new HTable(conf, tableName);
+
+
+
+        return hTableRow;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+
+
+    }
 }
 
